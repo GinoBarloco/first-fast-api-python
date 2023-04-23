1a1c6ce7c5503c84dccf43c8dbbc2fc1e8df3768
Gino <>
2023-04-23 20:30:49 -0300

    Fix NoneType for Optional

--- db/models/user.py
+++ db/models/user.py
@@ -3,6 +3,6 @@ from typing import Optional
 
 
 class User(BaseModel):
-    id: str | Optional[str]  # MongoDB toma los ID como Strings, y lo marco None porque MongoDB lo inserta.
+    id: Optional[str]  # MongoDB toma los ID como Strings, y lo marco None porque MongoDB lo inserta.
     username: str
     email: str
