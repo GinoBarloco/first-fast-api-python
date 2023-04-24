e30bf2e828b9b34d51addbcf713d8d29241dc60c
Gino <>
2023-04-23 21:20:52 -0300

    pprint test

--- db/schemas/user.py
+++ db/schemas/user.py
@@ -1,3 +1,6 @@
+import pprint
+
+
 def user_schema(user) -> dict:
     schema_dict = {"id": str(user["_id"]),  # user["_id"] es un objeto, no es str porque romperia la clase User()
                    "username": user["username"],
@@ -7,4 +10,4 @@ def user_schema(user) -> dict:
 
 
 def users_schema(users) -> list:
-    return [user_schema(user) for user in users]
+    return [pprint.pprint(user_schema(user)) for user in users]
